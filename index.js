ec6b0a842765afb3ebf5d7ff831aaa67f4aa6a6c
YuboChen20 <100442328+YuboChen20@users.noreply.github.com>
2022-12-01 21:00:31 +0100

    fef

--- index.js
+++ index.js
@@ -4,21 +4,25 @@ const fs=require("fs")
 app.use(express.static("public"))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+const mongojs = require('mongojs')
+const db = mongojs('mongodb://127.0.0.1:27017/test', ['task'])
 
 
-/*app.get("/", (req,res)=>{
-    res.send("hi M")
-})
-*/
 app.get("/json", (req,res)=>{
-    fs.readFile("public/data.txt", function(er,datos){
-        if(er){
-        }
+    var datos=[]
+    db.task.find({}).toArray(function(err, result) {
+        if (err){console.log("Error al bajar")}
+        else {
+        result.forEach(t=>{
+            datos.push({tarea : t.Task})
+        })
         console.log("Se ha bajado las tareas");
-
-        console.log(JSON.parse(datos))
-        res.json(JSON.parse(datos) )
+        console.log(datos)
+        res.json(datos)
+       }
     })
+    
+    
 
 })
 
@@ -26,15 +30,22 @@ app.get("/json", (req,res)=>{
 
 
 app.post("/",(req,res)=>{
-    var text = JSON.stringify(req.body)
-    fs.writeFile("public/data.txt", text, function(er){
-        if(er){
-        }
-        console.log("Se han subido las tareas");
-        console.log(JSON.parse(text));
-    })
+    var tareas = req.body
+    console.log("Tareas subidas:");
+    db.task.remove({})
+    if (tareas.length>0)
+        tareas.forEach(task => {
+            db.task.insertOne({"Task":task.tarea},(err,doc)=>{
+                if(err){ 
+                }else{
+                    console.log(task);
+                    
+                }
 
-})
+         
+    })
+})})
+
 app.listen(3000,function(){console.log("Servidor lanzado en el puerto 3000")})
 
 
